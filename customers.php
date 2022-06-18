030a8910641ab4f782f3b43fd9b8daf6ad632a3a
Srishti Mishra <srimishra16@gmail.com>
2022-06-18 08:28:40 +0530

    Initial Commit

---
+++ customers.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our Customers</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/table.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <style type="text/css">
+      button{
+        transition: 1.5s;
+      }
+      button:hover{
+        color: black;
+      }
+    </style>
+</head>
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Sparks bank </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="transferhistory.php">Transfer History</a>
+        </li>
+    
+
+      
+        
+</div>
+
+
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+
+<body style="background-image: url(https://il7.picdn.net/shutterstock/videos/8093134/thumb/8.jpg);" >
+<?php
+    include 'config.php';
+    $sql = "SELECT * FROM users";
+    $result = mysqli_query($conn,$sql);
+?>
+
+
+
+
+<div class="container">
+        <h2 class="text-center pt-5" style="color : black;">Our Customers</h2>
+        <br>
+            <div class="row">
+                <div class="col">
+                    <div class="table-responsive-sm">
+                    <table style="background-color: #4B5D67;" class="table table-hover table-sm table-striped table-condensed table-bordered" border="1">
+                        <thead style="color : black;">
+                            <tr>
+                            <th scope="col" class="text-center py-2">Account no.</th>
+                            <th scope="col" class="text-center py-2">Name of Holder</th>
+                            <th scope="col" class="text-center py-2">E-Mail</th>
+                            <th scope="col" class="text-center py-2"> Balance( Rs.)</th>
+                            <th scope="col" class="text-center py-2">Transfer Money</th>
+                            
+                            </tr>
+                        </thead>
+                        <tbody>
+                <?php 
+                    while($rows=mysqli_fetch_assoc($result)){
+                ?>
+                    <tr style="color : black;">
+                        <td class="py-2"><?php echo $rows['id'] ?></td>
+                        <td class="py-2"><?php echo $rows['name']?></td>
+                        <td class="py-2"><?php echo $rows['email']?></td>
+                        <td class="py-2"><?php echo $rows['balance']?></td>
+                        <td><a href="selecteduserdetail.php?id= <?php echo $rows['id'] ;?>"> <button type="button" class="btn" style="background-color : #789395;">Transfer money</button></a></td> 
+                    </tr>
+                <?php
+                    }
+                ?>
+            
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
+            </div> 
+         </div>
+         <footer class="text-center mt-5 py-2">
+         <p> @Copyright 2022 <b>All rights reserved</b> </br>Sparks Group</p>
+        </footer>
+         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script> 
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Welcome to Sparks bank</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+    <!-- Navigation bar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Sparks Bank </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="customers.php">Our Customers</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="transferhistory.php">Transfer History</a>
+                    </li>
+
+
+            </div>
+
+
+            </ul>
+
+        </div>
+        </div>
+    </nav>
+
+    <!-- Caraousel-->
+
+    <div id="demo" class="carousel slide" data-ride="carousel">
+
+        <!-- Indicators -->
+        <ul class="carousel-indicators">
+            <li data-target="#demo" data-slide-to="0" class="active"></li>
+            <li data-target="#demo" data-slide-to="1"></li>
+            <li data-target="#demo" data-slide-to="2"></li>
+        </ul>
+
+        <!-- The slideshow -->
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images/3.jpg" alt="Easy Transfer" width="1600" height="600">
+                <div class="carousel-caption">
+                    <h3 style="color:black;">Easy Money Transfer</h3>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/4.jpg" alt="Quick Deposit" width="1600" height="600">
+                <div class="carousel-caption  d-none d-md-block">
+                    <h3 style="color: black;">Quick Personal Loans</h3>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/2.jpg" alt="Low interests" width="1600" height="600">
+                <div class="carousel-caption">
+                    <h3 style="color:white;">Customer Satisfaction</h3>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Left and right controls -->
+        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#demo" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </a>
+    </div>
+
+
+
+    <!-- About us-->
+
+    <section class="my-5">
+        <div class="py-5">
+            <h3 class="text-center" style= "font-size:60px;"  >About Us</h3>
+        </div>
+
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-6 col-12">
+                    <img src="images/1.jpg" class="img-fluid">
+                </div>
+                <div class="col-lg-6 col-md-6 col-12">
+                    <h1 style= "font-size:45px; text-align: center">Sparks Bank</h1>
+                    <p1 style= "font-size:25px;"> Welcome! We are pleased that you have chosen Sparks Foundation Bank.
+                        Our aim is to offer easy and fast transfer and maximum customer satisfaction and provide
+                        financial facility.
+                        To ensure data transparency and bank credibility, each customer also has access to transaction
+                        history and also you can see all your customer data hoping you respect the privacy of each
+                        user.
+                </div>
+                <div class="col-lg-6 col-md-6 col-12">
+                    <h1 style= "font-size:45px; text-align: center">Make<br>Easy Transactions</h1>
+
+                </div>
+                <div class="col-lg-6 col-md-6 col-12">
+                    <img src="https://i.pinimg.com/originals/0e/d8/4a/0ed84aa877fedf76d84cc1080cc78958.jpg " class="img-fluid" width="1600" height="520">
+                </div>
+                <div class="col-lg-6 col-md-6 col-12">
+                    <img src="https://www.wadeharman.com/wp-content/uploads/2017/05/customer-service-1641724_1280-1.jpg" class="img-fluid">
+                </div>
+                <div class="col-lg-6 col-md-6 col-12">
+                    <h1 style= "font-size:45px;">Always Available<br> At Your Service</h1>
+
+                </div>
+
+                
+
+
+            </div>
+        </div>
+
+    </section>
+
+    <footer class="text-center mt-2 py-5">
+        <p> @Copyright 2022 <b>All rights reserved</b> </br>Sparks Group</br>Project By Srishti</p>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
